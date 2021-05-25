0183926287d10850b899e0d903fd562c72f4038e
subhadeep <subhadeepc19@gmail.com>
2021-05-25 23:47:54 +0530

    tried changing colors

--- src/Grid.js
+++ src/Grid.js
@@ -2,12 +2,29 @@ import React, { useState } from 'react'
 import './Grid.css'
 import useStyles from './Grid.styles';
 
+const initialCells = Array.from({ length: 40}, () => ({
+    on: false,
+    color: 'white',
+}))
+
 export default function Grid() {
     const classes = useStyles()
-    const [cells] = useState( Array.from({ length: 10000}));
+    const [cells, setCells] = useState(initialCells);
+    const updateCell = (i) => () => {
+        setCells(cells.map((cell, cellIndex) => {
+            if (cellIndex === i) {
+                //set to current COllor
+                return {
+                    on: true,
+                    color: 'black',
+                }
+            }
+            return cell;
+        }))
+    };
     return (
         <div className = {classes.grid}>
-            {cells.map(() => <div className={classes.cell}></div>)}
+            {cells.map((_, i) => <div key={i} style = {{ background:cells.on ? cells.color: '#fffff' }} className={classes.cell} onClick = {updateCell(i)}></div>)}
         </div>
     )
 }
--- src/Grid.styles.js
+++ src/Grid.styles.js
@@ -3,13 +3,13 @@ import { createUseStyles } from 'react-jss'
 export default createUseStyles({
   grid: {
     display: 'grid',
-    gridTemplateRows: 'repeat(100, 1fr)',
-    gridTemplateColumns: 'repeat(100, 1fr)',
+    gridTemplateRows: 'repeat(5, 1fr)',
+    gridTemplateColumns: 'repeat(8, 1fr)',
     width: '70vw',
     height: '50vw',
     outline: '1px dotted black',
     position: 'relative',
-    left : '25%'
+    left : '1.5625%'
   },
   cell: {
     cursor:'pointer',

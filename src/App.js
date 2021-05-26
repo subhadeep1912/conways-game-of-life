289c6ec4dbdf6976f93ccdc81c59005aad918deb
subhadeep <subhadeepc19@gmail.com>
2021-05-26 12:39:15 +0530

    gotta change the layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,104 @@
+import { useCallback, useState, useRef} from 'react';
 import './App.css';
 import ButtonGroup  from './ButtonGroup'
 import Grid from './Grid'
-import useStyles from './App.styles'
+import produce from 'immer'
 
+const numRows = 50;
+const numCols = 50;
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+]
 
 function App() {
-  const classes = useStyles();
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i< numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+    return rows
+  })
+
+
+
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return
+    }
+
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for (let i =0; i< numRows; i++) {
+        for (let k = 0; k < numCols; k++) {
+            let neighbours = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbours += g[newI][newK]
+              }
+            })
+
+            if (neighbours < 2 || neighbours > 3) {
+              gridCopy[i][k] = 0
+            } else if (g[i][k] === 0 && neighbours ===3 ) {
+              gridCopy[i][k] =1;
+            }
+          }
+        }
+      })
+    })
+    
+    /// simulate
+    
+    setTimeout(runSimulation, 100);
+  }, [])
+  
 
   return (
-    <div className = {classes.app}>
-      <ButtonGroup />
-      <Grid />
+    <>
+    <button onClick={() =>{
+        setRunning(!running)
+        if( !running ) {
+          runningRef.current = true
+          runSimulation();
+        } 
+    }}>{running ? 'stop' : 'start'}</button>
+    <div  style= {{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`,
+    }}>
+      {grid.map((rows, i) => rows.map((col,k) => <div 
+      key = {`${i} - ${k}`}
+      onClick = {() => {
+        const newGrid = produce(grid, gridCopy => {
+          gridCopy[i][k] = grid[i][k] ? 0 : 1;
+        })
+        setGrid(newGrid)
+      }}
+      style = {
+        {
+          width : 20,
+          height: 20,
+          backgroundColor: grid[i][k] ? 'pink' : undefined,
+          border: "solid 1px black"
+        }
+      }/>))}
     </div>
+    </>
   );
 }
 
--- src/Grid.js
+++ src/Grid.js
@@ -8,23 +8,10 @@ const initialCells = Array.from({ length: 100}, () => ({
 }))
 
 export default function Grid() {
-    const classes = useStyles()
-    const [cells, setCells] = useState(initialCells);
-    const updateCell = (i) => () => {
-        setCells(cells.map((cell, cellIndex) => {
-            if (cellIndex === i) {
-                //set to current COllor
-                return {
-                    on: true,
-                    color: 'black',
-                }
-            }
-            return cell;
-        }))
-    };
+    
     return (
-        <div className = {classes.grid}>
-            {cells.map((cell, i) => <div key={i} style = {{ background:cell.on ? cell.color: '#fffff' }} className={classes.cell} onClick = {updateCell(i)}></div>)}
+        <div>
+
         </div>
     )
 }
